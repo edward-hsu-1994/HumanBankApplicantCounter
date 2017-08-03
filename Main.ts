8cda30e8db53cddf714cb1ef88152cffe1d42e3a
徐培堯 <st89382000@gmail.com>
2017-08-03 13:43:02 +0800

    實作等候readyState的waitLoading方法與等候指定秒數方法

--- Main.ts
+++ Main.ts
@@ -5,16 +5,31 @@ interface String {
 String.prototype.innerString = function (start: string, end: string): string {
     let index = this.indexOf(start);
     if (index < 0) return null;
-    let result:string = this.substring(index + start.length);
+    let result: string = this.substring(index + start.length);
     index = result.indexOf(end);
     if (index < 0) return null;
     return result.substring(0, index);
 }
 
-class App{
+class App {
     public static counter: IBank;
 
-    public static parseHTML(htmlString:string) :HTMLDocument {
+    public static async sleep(s):Promise<void>{
+        return new Promise<void>((res,rej)=>{
+            setTimeout(res, s);
+        });        
+    }
+    
+    public static async waitLoading(): Promise<void> {
+        return new Promise<void>((res, rej) => {
+            if(document.readyState=="complete")res();
+            document.onreadystatechange = function (a) {
+                if(document.readyState!="complete")return;
+                res();
+            }
+        });
+    }
+    public static parseHTML(htmlString: string): HTMLDocument {
         return new DOMParser().parseFromString(htmlString, "text/html");
     }
 }
